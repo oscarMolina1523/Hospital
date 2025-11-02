d00c720f99dc35afa52b1d780e7f5102ffb86076
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-11-02 08:38:10 -0600

    feat: add calendar in appointment page

--- src/pages/Appointment.tsx
+++ src/pages/Appointment.tsx
@@ -1,9 +1,31 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
+import { Calendar } from "@/components/ui/calendar";
 
 const AppointmentPage: React.FC = () => {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
+
   return (
-    <div>
-      appointment<div>minimo</div>
+    <div className="p-4 rounded-2xl bg-white w-full flex flex-col gap-2">
+      <div className="w-full flex flex-row items-center justify-between">
+        <div>
+          <p className="text-[#0f172a] text-[1.25rem] leading-7">Citas</p>
+        </div>
+        <div>
+          <Button className="bg-sky-600 text-white">Nueva cita</Button>
+        </div>
+      </div>
+      <div className="flex flex-row gap-2">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-lg border w-1/3"
+        />
+        <div className="border rounded-2xl p-4 border-gray-300">
+          <p className="font-medium leading-2">citas del día</p>
+        </div>
+      </div>
     </div>
   );
 };
