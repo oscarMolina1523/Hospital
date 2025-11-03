d40fc1528cad2bc3b671228d7f077834deff41e6
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-11-03 12:30:54 -0600

    feat: add page login and register

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col gap-4">
       <h2 className="text-[1.3rem]">Iniciar Sesión</h2>
@@ -16,13 +18,13 @@ const LoginPage: React.FC = () => {
       </div>
       <Button className="bg-blue-600">Iniciar Sesion</Button>
       <div className="flex flex-row gap-2 w-full text-surface-neutral items-center">
-        <div className="w-1/2 h-[1px] bg-black"></div>
+        <div className="w-1/2 h-px bg-black"></div>
         <label className="text-surface-neutral">or</label>
-        <div className="w-1/2 h-[1px] bg-black"></div>
+        <div className="w-1/2 h-px bg-black"></div>
       </div>
       <div className="flex flex-row gap-2 w-full text-end items-center justify-center">
-        <label>Don't have an account?</label>
-        <a  className="text-ocean-sky-blue hover:underline">Registrarse</a>
+        <label>No tienes cuenta?</label>
+        <a onClick={() => navigate("/auth/register")} className="text-ocean-sky-blue hover:underline">Registrarse</a>
       </div>
     </div>
   );
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,53 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const RegisterPage: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      register<>minimo</>
+    <div className="flex flex-col gap-4">
+      <h2 className="text-[1.3rem]">Registrarse</h2>
+      <div className="flex flex-col items-start min-w-[20rem]">
+        <label>Correo electrónico</label>
+        <Input
+          className="bg-gray-200 focus:bg-white"
+          type="email"
+          placeholder="ej. oscar@gmail.com"
+        />
+      </div>
+      <div className="flex flex-col items-start min-w-[20rem]">
+        <label>Contraseña</label>
+        <Input
+          className="bg-gray-200 focus:bg-white"
+          type="password"
+          placeholder="ej. *********"
+        />
+      </div>
+      <div className="flex flex-col items-start min-w-[20rem]">
+        <label>Confirmar contraseña</label>
+        <Input
+          className="bg-gray-200 focus:bg-white"
+          type="password"
+          placeholder="ej. *********"
+        />
+      </div>
+      <Button className="bg-blue-600">Registrarse</Button>
+      <div className="flex flex-row gap-2 w-full text-surface-neutral items-center">
+        <div className="w-1/2 h-px bg-black"></div>
+        <label className="text-surface-neutral">or</label>
+        <div className="w-1/2 h-px bg-black"></div>
+      </div>
+      <div className="flex flex-row gap-2 w-full text-end items-center justify-center">
+        <label>Ya tienes cuenta?</label>
+        <a
+          onClick={() => navigate("/auth/register")}
+          className="text-ocean-sky-blue hover:underline"
+        >
+          Registrarse
+        </a>
+      </div>
     </div>
   );
 };
